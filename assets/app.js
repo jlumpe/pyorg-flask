ddb2c52b81d25305f719253da0106b3d11fc0d58
Jared Lumpe <mjlumpe@gmail.com>
2019-06-21 09:54:02 -0700

    Update CSS ans JS for HTML export changes

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
 
+    // Toggle sidebar menu expansion on click
     $('.sidebar li.menu-collapse > a').click(function () {
         console.log('clicked');
         $(this).parent().toggleClass('expanded');
     });
 
+    // Indeterminate state on checkboxes can only be set thru JS
+    $('.org-item.org-checkbox-trans > .org-checkbox').each(function () {
+        $(this).prop('indeterminate', true);
+    });
 });
