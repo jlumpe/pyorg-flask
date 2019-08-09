7602c94ce43619c5a07bc296597c49f041ce5200
Jared Lumpe <mjlumpe@gmail.com>
2019-08-09 09:40:27 -0700

    Add environment option to CLI

--- pyorg_flask/cli.py
+++ pyorg_flask/cli.py
@@ -7,9 +7,12 @@ from .factory import create_app
 
 
 @click.group(cls=FlaskGroup, create_app=create_app)
+@click.option('-e', '--env', help='Set the Flask environment. Valid values are "production" (default) and "development".')
 @click.option('--debug', is_flag=True, help='Run application in debug mode')
 @click.pass_context
-def cli(ctx, debug):
+def cli(ctx, env, debug):
 	"""Management script for the pyorg-flask application."""
+	if env:
+		os.environ['FLASK_ENV'] = env
 	if debug:
 		os.environ['FLASK_DEBUG'] = '1'
