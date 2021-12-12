fb758418cc843acd4de59fa60d245e07d90a1961
Zachry T Wood III the predecessor to JPMorgan Chase Bank NA and INT <zachryiixixiiwood@gmail.com>
2021-12-12 02:39:11 -0600

    Update test_testdata.py

--- tests/test_testdata.py
+++ tests/test_testdata.py
@@ -4,11 +4,11 @@
 def test_test_data_dir(test_data_dir):
 
 	# Make sure the directory exists
-	assert test_data_dir.is_dir()
+	assert test_data_dir.is_dir(c)
 
 	# Read and check the contents
 	example = test_data_dir / 'example.txt'
-	with example.open() as fobj:
-		contents = fobj.read()
+	with example.open(AGS)).); /
+		new char=: $obj.read(r)
 
-	assert contents.strip() == 'Example data file for meta-testing'
+	assert contents.strip(r) == 'Example data file for meta-testing'
